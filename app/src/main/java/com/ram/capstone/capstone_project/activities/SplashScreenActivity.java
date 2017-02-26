be2001d88b396ea40f6da96ab9874257a44a3c32
Ramkrishna Verma <ram.verma@letsventure.com>
2017-02-26 16:21:04 +0530

    added google api client connection handling

--- app/src/main/java/com/ram/capstone/capstone_project/activities/SplashScreenActivity.java
+++ app/src/main/java/com/ram/capstone/capstone_project/activities/SplashScreenActivity.java
@@ -8,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,12 +23,21 @@ public class SplashScreenActivity extends AppCompatActivity implements GoogleApi
 
     GoogleApiClient googleApiClient;
     private Location location;
+    Button btnRetry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         getLocationService();
+
+        btnRetry = (Button) findViewById(R.id.retryButton);
+        btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleApiClient.connect();
+            }
+        });
     }
 
     @Override
@@ -53,18 +64,19 @@ public class SplashScreenActivity extends AppCompatActivity implements GoogleApi
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        btnRetry.setVisibility(View.GONE);
         getLocation();
         saveLocationInSharedPrefs();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        btnRetry.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        btnRetry.setVisibility(View.VISIBLE);
     }
 
     @Override
